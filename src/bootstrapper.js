4d5ca8432069d744ce5884d67ff5facc4bcd890f
leseulsteve <leseulsteve@gmail.com>
2015-04-19 10:34:00 -0400

    no message

---
+++ src/bootstrapper.js
@@ -0,0 +1,19 @@
+var fs = require('fs'),
+	path = require('path'),
+	_ = require('lodash-node'),
+	nodeModulesDir = './node_modules';
+
+function getNodeModuleDirectories() {
+	return fs.readdirSync(nodeModulesDir).filter(function(file) {
+		return fs.statSync(path.join(nodeModulesDir, file)).isDirectory();
+	});
+}
+
+exports.init = function(app) {
+	var customModulesDirectories = _.filter(getNodeModuleDirectories(), function(dirName) {
+		return _.startsWith(dirName, 'leseulsteve');
+	});
+	_.forEach(customModulesDirectories, function(customModulesDirectory) {
+		require(customModulesDirectory)(app);
+	});
+};
