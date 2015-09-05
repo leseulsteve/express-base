c8e409ea729cc2cd119b27ed77943de2e699be3a
Steve Boisvert <leseulsteve@gmail.com>
2015-09-05 13:11:43 -0400

    Ajout d'un service de mail

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var express = require('express'),
   methodOverride = require('method-override'),
   bodyParser = require('body-parser'),
@@ -5,54 +7,57 @@ var express = require('express'),
   http = require('http'),
   helmet = require('helmet');
 
-function BaseApp() {
+var BaseApp = function() {};
 
-  return {
+BaseApp.prototype.init = function(config, callback) {
 
-    init: function(config, callback) {
+  this.config = config;
 
-      // Initialize express app
-      var app = express()
-      app.set('server', http.createServer(app));
+  // Initialize express app
+  this.app = express()
+  this.app.set('server', http.createServer(app));
 
-      app.use(morgan('dev'));
+  this.app.use(morgan('dev'));
 
-      // Use helmet to secure Express headers
-      app.use(helmet.xframe());
-      app.use(helmet.xssFilter());
-      app.use(helmet.nosniff());
-      app.use(helmet.ienoopen());
-      app.disable('x-powered-by');
+  // Use helmet to secure Express headers
+  this.app.use(helmet.xframe());
+  this.app.use(helmet.xssFilter());
+  this.app.use(helmet.nosniff());
+  this.app.use(helmet.ienoopen());
+  this.app.disable('x-powered-by');
 
-      // Enable CORS
-      app.use(function(req, res, next) {
-        res.header('Access-Control-Allow-Origin', '*');
-        res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-        res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Authorization, If-Modified-Since');
-        next();
-      });
+  // Enable CORS
+  this.app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Authorization, If-Modified-Since');
+    next();
+  });
 
-      app.options('*', function(req, res) {
-        res.sendStatus(200);
-      });
+  this.app.options('*', function(req, res) {
+    res.sendStatus(200);
+  });
 
-      app.use(bodyParser.urlencoded({
-        limit: '50mb',
-        extended: true
-      }));
+  this.app.use(bodyParser.urlencoded({
+    limit: '50mb',
+    extended: true
+  }));
 
-      app.use(bodyParser.json({
-        limit: '50mb'
-      }));
+  this.app.use(bodyParser.json({
+    limit: '50mb'
+  }));
 
-      app.use(methodOverride());
+  this.app.use(methodOverride());
 
-      // Start the app by listening on <port>
-      app.get('server').listen(config.port, function() {
-        callback(app);
-      });
-    }
-  };
-}
+  // Start the app by listening on <port>
+  this.app.get('server').listen(config.port, function() {
+    callback(app);
+  });
+};
+
+BaseApp.prototype.setMailerService = function(mailer) {
+  var mailer = require('./lib/mailer')(app);
+  mailer.init(config.mailer, mailer);
+};
 
 module.exports = new BaseApp();
